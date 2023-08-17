c1c1f6413622f4550b5d0378c41f09c0b6865866
TheConsciousness <jordanabrinkman@gmail.com>
2023-08-17 12:32:33 -0400

    CPU and Memory endpoints working.

--- index.js
+++ index.js
@@ -1,23 +1,93 @@
-const express = require('express');
-const app = express();
-const os = require("os");
+const port = process.env.PORT || 6420;
+const http = require('http');
+const os = require('os');
+const { memoryUsage } = require('process');
 
-app.get('/', (req,res)=> {
-    console.log(req.socket);
-    res.send("hello");
-});
+var memObject = {};
+var cpuOutput = {};
+cpuOutput.CPU = {};
 
-app.get('/memory', (req,res)=> {
-    //const output = '{"Memory": ["Free": '+os.freemem()+'],["Total": '+os.totalmem()+']}';
-    var output = [];
-    output.push({"Free":os.freemem()});
-    output.push({"Total":os.totalmem()});
-    res.setHeader('content-type', 'application/json');
-    res.send(output);
-});
+http.createServer((req,res)=> {
 
-const server = app.listen(8081, ()=> {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log("Server is listening at: " + host + ":" + port);
-});
+    function getMemory() {
+        memObject.Memory = {};
+        memObject.Memory.Free = os.freemem();
+        memObject.Memory.Total = os.totalmem();
+        memObject.Memory.Percentage = Math.round((os.freemem()/os.totalmem())*100);
+    }
+    function getCPU() {
+
+    }
+
+    if (req.url === "/memory") {
+        
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200;
+        getMemory();
+        res.end(JSON.stringify(memObject, null, 2));
+    }
+    else if (req.url === "/cpu") {
+        
+        res.setHeader('Content-Type', 'application/json');
+
+        /* cpuObject = {};
+        cpuObject.CPU = {};
+        //cpuObject.CPU.What = os.cpus();
+
+        os.cpus().forEach(cpu => {
+            ourTimes = Object.values(cpu.times).reduce((a, b) => a + b, 0);
+            console.log(ourTimes);
+        });
+
+        const usage = process.cpuUsage();
+        const currentCPUUsage = (usage.user + usage.system) * 1000;
+        // Find out the percentage used for this specific CPU
+        const perc = currentCPUUsage / ourTimes[0] * 100;
+        console.log(usage); */
+
+        getCPU();        
+        const { exec } = require("child_process");
+        exec('top -l 1 | grep -E "^CPU"', (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                res.statusCode = 500;
+                res.end(`${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                res.statusCode = 500;
+                res.end(`${stderr}`);
+                return;
+            }
+            res.statusCode = 200;
+
+            let splittin = stdout.split(':');
+            splittin = splittin[1].split(",");
+
+            cpuOutput.CPU.User = splittin[0].replace(" user", "").trim();
+            cpuOutput.CPU.System = splittin[1].replace(" sys", "").trim();
+            cpuOutput.CPU.Used = (parseFloat(cpuOutput.CPU.User) + parseFloat(cpuOutput.CPU.System)) + "%";
+            cpuOutput.CPU.Free = splittin[2].replace(" idle \n", "").trim();
+            console.log(cpuOutput);
+        });
+
+        console.log(cpuOutput);
+        res.end(JSON.stringify(cpuOutput, null, 2));
+    }
+    else if (req.url === "/") {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.statusCode = 200;
+        res.end("<a href=/cpu>CPU</a><br/><a href=/memory>Memory</a>");
+    }
+    else if (req.url === "/favicon.ico") {
+        res.end("");
+    }
+    else if (req.url === "/both") {
+        fetch("http://127.0.0.1:6420/cpu")
+        .then((response) => response.text())
+        .then((body) => {
+            res.end(body);
+        });
+    }
+}).listen(port);
