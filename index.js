ea35f3f1a236b38ed0cc8fbc24297cbc19a9d042
TheConsciousness <jordanabrinkman@gmail.com>
2023-08-21 15:06:58 -0400

    Partial progress on classes

--- index.js
+++ index.js
@@ -10,28 +10,109 @@ class PC {
     HDDs = {};
     Memory = {};
     CPU = {};
+
     constructor(name) {
         this.Hostname = name;
     }
-    freshCPU() {
+    async freshCPU() {
+        try {
+            //throw new Error('Couldnt retrieve CPU stats.');
+            const { stdout } = await require('util').promisify(require('child_process').exec)('top -l 1 | grep -E "^CPU"');
+            let splittin = stdout.split(':');
+            splittin = splittin[1].split(",");
 
+            this.CPU.User = splittin[0].replace(" user", "").trim();
+            this.CPU.System = splittin[1].replace(" sys", "").trim();
+            this.CPU.Used = Math.round((parseFloat(this.CPU.User) + parseFloat(this.CPU.System))) + "%";
+            this.CPU.Free = splittin[2].replace(" idle \n", "").trim();
+            this.CPU.LastRefresh = formatDate(Date.now());
+
+            return {CPU: this.CPU};
+        }
+        catch (err) {
+            console.log(`Catch: ${err}`);
+            this.CPU = err.message;
+            return {CPU: this.CPU};
+        }
     }
-    freshHDDs() {
-
+    async freshHDDs() {
+        try {
+            const disks = await nodeDiskInfo.getDiskInfo();
+            this.HDDs = 
+                {
+                HDDs: disks.map(disk => {
+                    return {
+                        ...disk,
+                        _available: formatBytes(disk._available),
+                        _blocks: formatBytes(disk._blocks),
+                        _used: formatBytes(disk._used)
+                    }
+                }),
+                LastRefresh: formatDate(Date.now())
+            }
+            return this.HDDs;
+        } catch (err) {
+            console.log(`PC.freshHDDs(): ${err}`);
+            this.HDDs = err.message;
+            return this.HDDs;
+        }
     }
     freshMemory() {
         try {
             this.Memory.Free = formatBytes(os.freemem());
             this.Memory.Total = formatBytes(os.totalmem());
             this.Memory.PercentUsed = Math.round((os.freemem()/os.totalmem())*100) + "%";
-            return this.Memory;
+            this.Memory.LastRefresh = formatDate(Date.now());
+            return {Memory: this.Memory};
         } catch (err) {
             console.log(`PC.freshMemory(): ${err}`);
             this.Memory = err.message;
-            return this.Memory;
+            return {Memory: this.Memory};
         }
     }
+    async allStats() {
+        try {
+            //throw new Error("CPU");
+            var cpuData = await this.freshCPU();
+        } catch (cpuerr) {
+            var cpuData = {CPUs:cpuerr.message};
+        }
+        try {
+            //throw new Error("HDD");
+            var hddData = {};
+            hddData.HDDs = await nodeDiskInfo.getDiskInfo();
+            hddData.HDDs.push({LastRefresh: formatDate(Date.now())});
+        } catch (hdderr) {
+            var hddData = {HDDs:hdderr.message,
+                LastRefresh: formatDate(Date.now())};
+        }
+
+        try {
+            var memData = await this.freshMemory();
+            //throw new Error("Memory");
+        } catch (memerr) {
+            memData = {Memory:memerr.message};
+        }
+
+        try {
+            throw new Error("All error!");
+            allObject = {
+                [os.hostname()]:{
+                    ...cpuData,
+                    ...memData,
+                    ...hddData
+                    
+                //  }).slice(0,-1) // Use this is the last value is garbage
+            }}
+        } catch (error) {
+            allObject[os.hostname()] = error.message;
+        }
+        return allObject;
+    }
 }
+
+
+const currentPC = new PC();
 
 var hddObject = {};
 var allObject = {};
@@ -46,41 +127,8 @@ cpuObject[os.hostname()].CPU = {};
 memObject[os.hostname()].Memory = {};
 hddObject[os.hostname()].HDDs = {};
 
-const getMemory = () => {
-    try {
-        //throw new Error('Couldnt retrieve memory stats.');
-        memObject[os.hostname()].Memory = {};
-        memObject[os.hostname()].Memory.Free = formatBytes(os.freemem());
-        memObject[os.hostname()].Memory.Total = formatBytes(os.totalmem());
-        memObject[os.hostname()].Memory.PercentUsed = Math.round((os.freemem()/os.totalmem())*100) + "%";
-
-        return memObject;
-    } catch (err) {
-        console.log(`Catch: ${err}`);
-        memObject[os.hostname()].Memory = err.message;
-        return memObject;
-    }
-    
-}
 const asyncGetCPU = async () => {
-    try {
-        //throw new Error('Couldnt retrieve CPU stats.');
-        const { stdout } = await require('util').promisify(require('child_process').exec)('top -l 1 | grep -E "^CPU"');
-        let splittin = stdout.split(':');
-        splittin = splittin[1].split(",");
-
-        cpuObject[os.hostname()].CPU.User = splittin[0].replace(" user", "").trim();
-        cpuObject[os.hostname()].CPU.System = splittin[1].replace(" sys", "").trim();
-        cpuObject[os.hostname()].CPU.Used = Math.round((parseFloat(cpuObject[os.hostname()].CPU.User) + parseFloat(cpuObject[os.hostname()].CPU.System))) + "%";
-        cpuObject[os.hostname()].CPU.Free = splittin[2].replace(" idle \n", "").trim();
-
-        return cpuObject
-    }
-    catch (err) {
-        console.log(`Catch: ${err}`);
-        cpuObject[os.hostname()].CPU = err.message;
-        return cpuObject;
-    }
+    
 }
 const formatBytes = (bytes, decimals = 2) => {
     if (bytes === 0) return '0 Bytes';
@@ -89,6 +137,16 @@ const formatBytes = (bytes, decimals = 2) => {
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+function formatDate(timestamp) {
+    const date = new Date(timestamp);
+    const formattedMonth = String(date.getMonth() + 1).padStart(2, '0');
+    const formattedDay = String(date.getDate()).padStart(2, '0');
+    const formattedYear = String(date.getFullYear() % 100).padStart(2, '0');
+    const formattedHours = String(date.getHours()).padStart(2, '0');
+    const formattedMinutes = String(date.getMinutes()).padStart(2, '0');
+    const formattedSeconds = String(date.getSeconds()).padStart(2, '0');
+    return `${formattedMonth}/${formattedDay}/${formattedYear} ${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
 }
 
 http.createServer(async (req,res)=> {
@@ -113,57 +171,19 @@ http.createServer(async (req,res)=> {
         })
     }
     else if (req.url === "/memory") {
-        const memData = getMemory();
-        res.end(JSON.stringify(memData, null, 2));
+        res.end(JSON.stringify(currentPC.freshMemory(), null, 2));
     }
 
     else if (req.url === "/cpu") {
-        const cpuData = await asyncGetCPU();
-        res.end(JSON.stringify(cpuData, null, 2));
+        res.end(JSON.stringify(await currentPC.freshCPU(), null, 2));
     }
 
     else if (req.url === "/hdd") {
-        try {
-            const disks = await nodeDiskInfo.getDiskInfo();
-            hddObject = {
-                [os.hostname()]:{
-                HDDs: disks.map(disk => {
-                    return {
-                        ...disk,
-                        _available: formatBytes(disk._available),
-                        _blocks: formatBytes(disk._blocks),
-                        _used: formatBytes(disk._used)
-                    }
-                })
-            }}
-        } catch (error) {
-            hddObject[os.hostname()].HDDs = error.message;
-        }
-        res.end(JSON.stringify(hddObject, null, 2));
+        res.end(JSON.stringify(await currentPC.freshHDDs(), null, 2));
     }
 
     else if (req.url === "/all") {
-        try {
-            const [cpuData, hddData] = await Promise.all([asyncGetCPU(), nodeDiskInfo.getDiskInfo()]);
-            const memData = getMemory();
-            allObject = {
-                [os.hostname()]:{
-                    ...cpuData[os.hostname()],
-                    ...memData[os.hostname()],
-                    HDDs: hddData.map(disk => {
-                        return {
-                            ...disk,
-                            _available: formatBytes(disk._available),
-                            _blocks: formatBytes(disk._blocks),
-                            _used: formatBytes(disk._used)
-                        }
-                    })
-                //  }).slice(0,-1) // Use this is the last value is garbage
-            }}
-        } catch (error) {
-            allObject[os.hostname()] = error.message;
-        }
-        res.end(JSON.stringify(allObject, null, 2));
+        res.end(JSON.stringify(await currentPC.allStats(), null, 2));
     }
 
     
